1ed287b1f7bcef387b89141bfcffbb4ae73752b1
OdehYazan <eng.yazanodeh@gmail.com>
2021-07-26 20:27:28 +0300

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.use(express.json())
 
 
 
-mongoose.connect("mongodb://emanmkhareez:eman12345@cluster0-shard-00-00.2xoym.mongodb.net:27017,cluster0-shard-00-01.2xoym.mongodb.net:27017,cluster0-shard-00-02.2xoym.mongodb.net:27017/recipes?ssl=true&replicaSet=atlas-zeqdyo-shard-0&authSource=admin&retryWrites=true&w=majority", {
+mongoose.connect('mongodb://localhost:27017/recipes', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -50,6 +50,7 @@ server.get('/recipes', GetRecipes);
 //http://localhost:3001/AddRecipe
 server.post('/AddRecipe/:email',addRecipeHandler);
 server.get('/GetFavData/:email',GetFavData)
+server.put('/updateRecipe/:email',updateRecipeFun)
 
 function GetFavData(req,res){
     const UserEmail=req.params.email
@@ -70,7 +71,6 @@ const NewPecipe=new myrecipeModel ({
 
 })
 NewPecipe.save()
-
 //   userrecipeModel.find({ email: Email }, (error, recipeData) => {
 //     if (error) {
 //         res.send(error, 'no favert')
@@ -95,6 +95,31 @@ NewPecipe.save()
 
 //     }
 // })
+}
+
+
+function updateRecipeFun (req,res){
+    
+    console.log('aaaaaa',req.body);
+    console.log('aaaaaa',req.params);
+
+    let {updateLabel,userEmail} = req.body;
+    let index = Number(req.params.email);
+
+    myrecipeModel.findOne({email:userEmail},(error,recipeData)=>{
+        if(error) res.send('error in finding the data')
+        else {
+            console.log(recipeData)
+            recipeData.recipes.splice(index,1,{
+                updateLabel:updateLabel
+            })
+            console.log(recipeData)
+            recipeData.save();
+            res.send(recipeData.recipes)
+            
+        }
+    })
+
 }
 
 
